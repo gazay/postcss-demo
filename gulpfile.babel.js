93e7f12ad901d7be305ddad788df3f4439554616
Alexey Gaziev <alex.gaziev@gmail.com>
2015-05-27 00:00:59 +0000

    Add independent build functions for each plugin

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,6 +23,71 @@ gulp.task('build', () => {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('build-separate', ['postcss-color-function', 'postcss-color-gray', 'postcss-color-hex-alpha',
+                             'postcss-color-hwb', 'postcss-color-rebeccapurple', 'postcss-conic-gradient'])
+
+// Build functions for each plugin independently
+
+gulp.task('postcss-color-function', () => {
+  return gulp.src('postcss-color-function/style.input.css')
+    .pipe(postcss([ require('postcss-color-function')() ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('./postcss-color-function'));
+});
+
+gulp.task('postcss-color-gray', () => {
+  return gulp.src('postcss-color-gray/style.input.css')
+    .pipe(postcss([ require('postcss-color-gray')() ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('./postcss-color-gray'));
+});
+
+gulp.task('postcss-color-hex-alpha', () => {
+  return gulp.src('postcss-color-hex-alpha/style.input.css')
+    .pipe(postcss([ require('postcss-color-hex-alpha')() ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('./postcss-color-hex-alpha'));
+});
+
+gulp.task('postcss-color-hwb', () => {
+  return gulp.src('postcss-color-hwb/style.input.css')
+    .pipe(postcss([ require('postcss-color-hwb')() ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('./postcss-color-hwb'));
+});
+
+gulp.task('postcss-color-rebeccapurple', () => {
+  return gulp.src('postcss-color-rebeccapurple/style.input.css')
+    .pipe(postcss([ require('postcss-color-rebeccapurple')() ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('./postcss-color-rebeccapurple'));
+});
+
+gulp.task('postcss-conic-gradient', () => {
+  return gulp.src('postcss-conic-gradient/style.input.css')
+    .pipe(postcss([ require('postcss-conic-gradient')() ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('./postcss-conic-gradient'));
+});
+
 // Lint
 
 gulp.task('lint', () => {
