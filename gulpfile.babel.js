b088d18dc6dd2f038b1949266c069dd4c597cd88
Alexey Gaziev <alex.gaziev@gmail.com>
2015-07-20 00:19:17 +0800

    Add gulp build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,26 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
+import postcss from 'gulp-postcss';
+import use from 'postcss-use';
+import rename from 'gulp-rename';
 
 // Default
 
 gulp.task('default', ['lint']);
+
+// Build
+
+gulp.task('build', () => {
+  return gulp.src(['**/*.input.css'])
+    .pipe(postcss([
+      use({ modules: ['postcss-color-function']})
+    ]))
+    .pipe(rename(path => {
+      path.basename = path.basename.replace('.input', '');
+      path.extname = '.out.css';
+    }))
+    .pipe(gulp.dest('.'));
+});
 
 // Lint
 
