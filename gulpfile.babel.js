2d364c6eca9c45fc6cf8f3ee99a34c2d4be542d7
Alexey Gaziev <alex.gaziev@gmail.com>
2015-07-29 15:31:35 +0000

    Add postcss-color-hex-alpha

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,7 +13,7 @@ gulp.task('default', ['lint']);
 gulp.task('build', () => {
   return gulp.src(['**/*.input.css'])
     .pipe(postcss([
-      use({ modules: ['postcss-color-function', 'postcss-color-gray']})
+      use({ modules: ['postcss-color-function', 'postcss-color-gray', 'postcss-color-hex-alpha']})
     ]))
     .pipe(rename(path => {
       path.basename = path.basename.replace('.input', '');
